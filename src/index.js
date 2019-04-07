8589b8aac9c8f98ff5e6e3b3c3934cc70289663b
ryotogashi <ryo.togashi.ca@gmail.com>
2019-04-07 06:05:26 -0700

    add plus and multiple function.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('hello world');
+
+const multiple = function(a, b) {
+    return a * b;
+}
+
+const plus = function(a, b) {
+    return a + b;
+}
+
+let s = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+console.log('plus 1 to 9: ' + s.reduce(plus));
+console.log('multiple 1 to 9: '+ s.reduce(multiple));
+
